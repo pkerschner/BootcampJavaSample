50d0c0f7822f89f4258b604c462acfd584a376e9
Philip Kerschner <pkerschner@zoominternet.net>
2022-03-01 12:23:47 -0500

    Initial commit

---
+++ src/com/maxtrain/java/instruction/Program.java
@@ -0,0 +1,10 @@
+package com.maxtrain.java.instruction;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the Program Class");
+	}
+
+}
